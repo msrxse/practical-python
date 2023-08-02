85f41718a5382857e2289e52e095e2b713036d01
Marcos Suarez <msrxse@gmail.com>
2023-08-02 20:30:20 +0100

    Lesson 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+building_height = 100
+actual_height = 0
+i = 0
+
+while i < 10:
+    building_height = round(building_height * 3 / 5, 4)
+    print(building_height)
+    i += 1
---
+++ Work/hello.py
@@ -0,0 +1,14 @@
+# hello.py
+#
+# Exercise 1.2
+print("Hello World")
+
+# The extra newline can be suppressed:
+
+print("Hello", end=" ")
+print("My name is", "Jake")
+
+# To read a line of typed user input, use the input() function:
+
+name = input("Enter your name:")
+print("Your name is", name)
