e53dd5258fe3439164734b73bbef76255994fe39
Marcos Suarez <msrxse@gmail.com>
2023-08-22 17:13:44 +0100

    Opening files

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
+import gzip
+
 # Exercise 1.27
+with open("Work/Data/portfolio.csv", "rt") as f:
+    headers = next(f)
+    result = 0
+    for line in f:
+        row = line.split(",")
+        result += int(row[1]) * float(row[2])
+    print(result)
+
+with gzip.open("Work/Data/portfolio.csv.gz", "rt") as f:
+    for line in f:
+        print(line, end="")
